ecdce64dca0ae96d6cd2616588e5c2f6b010e044
Tony <anthonygeorge140@gmail.com>
2026-04-29 00:55:38 +0300

    third update of the design

--- Vezzeta/Form2.Designer.cs
+++ Vezzeta/Form2.Designer.cs
@@ -17,6 +17,7 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.lblTitle = new System.Windows.Forms.Label();
             this.txtFilterValue = new System.Windows.Forms.TextBox();
             this.btnLoadData = new System.Windows.Forms.Button();
@@ -24,7 +25,9 @@
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
-         
+            // 
+            // lblTitle
+            // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
             this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
@@ -33,16 +36,22 @@
             this.lblTitle.Size = new System.Drawing.Size(479, 30);
             this.lblTitle.TabIndex = 4;
             this.lblTitle.Text = "Disconnected Mode (Manage Appointments)";
-        
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.txtFilterValue.Font = new System.Drawing.Font("Segoe UI", 11F);
             this.txtFilterValue.Location = new System.Drawing.Point(25, 70);
             this.txtFilterValue.Name = "txtFilterValue";
             this.txtFilterValue.Size = new System.Drawing.Size(250, 27);
             this.txtFilterValue.TabIndex = 3;
             this.txtFilterValue.Text = "Enter Patient ID to Filter";
-          
-            this.btnLoadData.BackColor = System.Drawing.Color.LightGray;
+            // 
+            // btnLoadData
+            // 
+            this.btnLoadData.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
             this.btnLoadData.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnLoadData.ForeColor = System.Drawing.Color.White;
             this.btnLoadData.Location = new System.Drawing.Point(290, 68);
             this.btnLoadData.Name = "btnLoadData";
             this.btnLoadData.Size = new System.Drawing.Size(150, 31);
@@ -50,7 +59,9 @@
             this.btnLoadData.Text = "Load Data";
             this.btnLoadData.UseVisualStyleBackColor = false;
             this.btnLoadData.Click += new System.EventHandler(this.btnLoadData_Click_1);
-          
+            // 
+            // btnSaveChanges
+            // 
             this.btnSaveChanges.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(102)))), ((int)(((byte)(204)))));
             this.btnSaveChanges.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
             this.btnSaveChanges.ForeColor = System.Drawing.Color.White;
@@ -60,23 +71,27 @@
             this.btnSaveChanges.TabIndex = 0;
             this.btnSaveChanges.Text = "SAVE CHANGES TO DATABASE";
             this.btnSaveChanges.UseVisualStyleBackColor = false;
-          
-         
+            // 
+            // dataGridView2
+            // 
             this.dataGridView2.Location = new System.Drawing.Point(25, 120);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.Size = new System.Drawing.Size(635, 280);
             this.dataGridView2.TabIndex = 1;
-       
-            this.BackColor = System.Drawing.Color.White;
+            // 
+            // Form2
+            // 
+            this.BackColor = System.Drawing.Color.AliceBlue;
             this.ClientSize = new System.Drawing.Size(684, 500);
             this.Controls.Add(this.btnSaveChanges);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.btnLoadData);
             this.Controls.Add(this.txtFilterValue);
             this.Controls.Add(this.lblTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form 2 - Disconnected Mode";
+            this.Text = " Disconnected Mode";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
